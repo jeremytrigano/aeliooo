25639439dcb91604a6e4246099cb39c1e1ed0d7b
jeremy.trigano <trigano.jeremy@gmail.com>
2019-09-12 14:49:36 +0200

    gg

--- flasksrv.py
+++ flasksrv.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +8,49 @@ CORS(app)
 @app.route('/')
 def index():
     return "Bienvenue chez Aeliooo"
+
+
+@app.route('/commandes', methods=['GET', 'POST', 'PUT'])
+def commandes():
+    # Le client récupère ses commandes:
+    # Requête GET @/commandes?nomClient=Baratoux
+    if request.method == 'GET':
+        if request.args.get('nomClient'):
+            nomClient = request.args.get('nomClient')
+            # select cmd str(nomClient)
+            # Réponse JSON {'commandes': [{'id': 12, 'etat': 4}, ...]}
+            return {'commandes': [{'id': 12, 'etat': 4}], 'nom': nomClient}
+        else:
+            return {'status': 'aucun client'}
+    # Le client passe une commandes:
+    # Requête POST @/commandes
+    elif request.method == 'POST':
+        # réception JSON {'nom': 'Baratoux', 'adresse': 'Rue machin', 'contenu': {}}
+        # Réponse JSON {'retour': ’OK’}
+        return {'retour': 'OK'}
+    # La cuisine met la commande à jour:
+    # Le livreur met la commande à jour:
+    # Requête PUT @/commandes
+    elif request.method == 'PUT':
+        # Réponse JSON {'id': 12, 'etat': 3}
+        return {'id': 12, 'etat': 3}
+    else:
+        return "Bienvenue chez Aeliooo - Les commandes "
+
+
+# La cuisine récupère ses commandes:
+# Requête GET @/commandes/cuisine
+@app.route('/commandes/cuisine')
+def commandesCuisine():
+    # Réponse JSON {'commandes': [{'id': 12, 'nom': 'Baratoux', 'etat': 4, 'contenu': {....}}, ...]}
+    return {'commandes': [{'id': 12, 'nom': 'Baratoux', 'etat': 4, 'contenu': {}}]}
+
+# Le livreur récupère ses livraisons:
+# Requête GET @/commandes/livreur
+@app.route('/commandes/livreur')
+def commandesLivreur():
+    # Réponse JSON {'commandes': [{'id': 12, 'nom': 'Baratoux', 'etat': 4}, ...]}
+    return {'commandes': [{'id': 12, 'nom': 'Baratoux', 'etat': 4}]}
 
 
 if __name__ == '__main__':
